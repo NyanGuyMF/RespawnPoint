da1d71d5e9467706fe7abc2887408d3b109eb460
Vasiliy <vasyokbelyivnew@gmail.com>
2020-03-04 04:36:35 +0500

    ooops, copy-paste license comment inssue

--- src/main/java/voidpointer/bukkit/respawnpoint/InfoMessage.java
+++ src/main/java/voidpointer/bukkit/respawnpoint/InfoMessage.java
@@ -1,18 +1,18 @@
 /*
- * This file is part of TemporalNicknameWhitelist Bukkit, Spigot and PaperSpigot compatable plug-in.
+ * This file is part of RespawnPoint Bukkit, Spigot and PaperSpigot compatable plug-in.
  *
- * TemporalNicknameWhitelist is free software: you can redistribute it and/or modify
+ * RespawnPoint is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * TemporalNicknameWhitelist is distributed in the hope that it will be useful,
+ * RespawnPoint is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with TemporalNicknameWhitelist. If not, see <https://www.gnu.org/licenses/>.
+ * along with RespawnPoint. If not, see <https://www.gnu.org/licenses/>.
  */
 package voidpointer.bukkit.respawnpoint;
 
